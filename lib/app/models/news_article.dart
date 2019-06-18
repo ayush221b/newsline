41ee18ac856e15f074f605cd1c70c925a2c9bd7a
Ayush Shekhar <ayush221b@gmail.com>
2019-06-18 18:19:40 +0530

    Created news service class, updated model and dbhelper to accomodate category, added a helper utility to check if internet is present or not

--- lib/app/models/news_article.dart
+++ lib/app/models/news_article.dart
@@ -1,58 +1,58 @@
 import 'package:meta/meta.dart';
 
 /// `NewsArticle` provides a representative data model class
-  /// which helps to parse the received json in response from NewsApi.org
-  /// `sourceId` : NewsApi Id(if present) of the source
-  /// `sourceName` : Name of Source
-  /// `author` : Author of article
-  /// `title` : Title of Article
-  /// `description` : Description about article
-  /// `url` : Url to original article
-  /// `urlToImage` : Link to the image, relevant to the article
-  /// `publishedAt` : Date and Time when the article was published in UTC(+000)
-  /// `content` : Content of article, truncated to 260 characters (Developer Plan Limit)
+/// which helps to parse the received json in response from NewsApi.org
+/// `sourceId` : NewsApi Id(if present) of the source
+/// `sourceName` : Name of Source
+/// `author` : Author of article
+/// `title` : Title of Article
+/// `description` : Description about article
+/// `url` : Url to original article
+/// `urlToImage` : Link to the image, relevant to the article
+/// `publishedAt` : Date and Time when the article was published in UTC(+000)
+/// `content` : Content of article, truncated to 260 characters (Developer Plan Limit)
+/// `category` : Category of article
 class NewsArticle {
-
   String sourceId;
   String sourceName;
-  String author; 
-  String title; 
-  String description; 
-  String url; 
-  String urlToImage; 
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
   DateTime publishedAt;
   String content;
+  String category;
 
-  NewsArticle({
-    @required this.sourceId,
-    @required this.sourceName,
-    @required this.author,
-    @required this.title,
-    @required this.description,
-    @required this.url,
-    @required this.urlToImage,
-    @required this.publishedAt,
-    @required this.content
-  });
+  NewsArticle(
+      {@required this.sourceId,
+      @required this.sourceName,
+      @required this.author,
+      @required this.title,
+      @required this.description,
+      @required this.url,
+      @required this.urlToImage,
+      @required this.publishedAt,
+      @required this.content,
+      this.category});
 
   /// Named Constructor which receives a map [responseMap] and parses
   /// it into a [NewsArticle] instance.
-  NewsArticle.fromMap(Map<String, dynamic> responseMap) {
-
-    sourceId = responseMap['source']['id'];
-    sourceName = responseMap['source']['name'];
-    author = responseMap['author'];
-    title = responseMap['title'];
-    description = responseMap['description'];
-    url = responseMap['url'];
-    urlToImage = responseMap['urlToImage'];
-    publishedAt = DateTime.parse(responseMap['publishedAt']);
-    content = responseMap['content'];
+  NewsArticle.fromMap(Map articleMap) {
+    sourceId = articleMap['source']['id'];
+    sourceName = articleMap['source']['name'];
+    author = articleMap['author'];
+    title = articleMap['title'];
+    description = articleMap['description'];
+    url = articleMap['url'];
+    urlToImage = articleMap['urlToImage'];
+    publishedAt = DateTime.parse(articleMap['publishedAt']);
+    content = articleMap['content'];
+    if (articleMap['category'] != null) category = articleMap['category'];
   }
 
-  /// Function to generate a `Map<String, dynamic>` from a `NewsArticle` instance. 
+  /// Function to generate a `Map<String, dynamic>` from a `NewsArticle` instance.
   Map<String, dynamic> toMap() {
-
     final Map<String, dynamic> data = new Map<String, dynamic>();
 
     data['sourceId'] = this.sourceId;
@@ -64,6 +64,8 @@ class NewsArticle {
     data['urlToImage'] = this.urlToImage;
     data['publishedAt'] = this.publishedAt.toString();
     data['content'] = this.content;
+
+    if (this.category != null) data['category'] = this.category;
 
     return data;
   }
--- lib/app/services/news_service.dart
+++ lib/app/services/news_service.dart
@@ -1,49 +1,140 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
+import 'package:newsline/app/database/dbhelper.dart';
 import 'package:newsline/app/models/news_article.dart';
 import 'package:http/http.dart' as http;
 import 'package:newsline/app/secret_key.dart';
+import 'package:newsline/app/services/internet-service.dart';
+import 'package:rxdart/subjects.dart';
 
-// enum to track the progress of News Articles
-enum NewsLoadState {
-  Loading,
-  Loaded,
-  Failed
-}
+// Utility to track the progress of News Articles
+enum NewsLoadState { Loading, Loaded, Vacant }
 
 /// `NewsService` will act as a utility class and perform all
 /// operations related to the NewsArticles.
 class NewsService extends ChangeNotifier {
+  /// Stores the master list of `NewsArticle` instances
   List<NewsArticle> _articlesList = [];
 
+  /// Getter for the news articles list
   List<NewsArticle> get articles {
     return _articlesList;
   }
 
-  Future getArticlesFromApi(
-      {int page = 1, String country = 'in', String query = '', String category=''}) async {
+  /// To enable listening to changes of `NewsLoadState`
+  PublishSubject<NewsLoadState> _newsLoadStateSubject = PublishSubject();
 
+  /// Getter for `NewsLoadState` PublishSubject
+  PublishSubject<NewsLoadState> get newsLoadState {
+    return _newsLoadStateSubject;
+  }
+
+  /// Instance of Database Helper class
+  var _dbhelper = DBHelper();
+
+  /// Fetch news articles from NewsApi.org, optionally accepts
+  /// the request query params.
+  Future getArticlesFromApi(
+      {String country = 'in', String query = '', String category = ''}) async {
     String baseUrl = 'https://newsapi.org/v2/top-headlines';
 
-    String requestParams = '?apiKey=${SecretKey.apiKey}' 
-        + '&country=$country'
-        + '&page=$page'
-        + '&q=$query'
-        + '&category=$category';
-    
+    String requestParams = '?apiKey=${SecretKey.apiKey}' +
+        '&country=$country' +
+        '&q=$query' +
+        '&category=$category';
+
+    // Update NewsLoadState to Loading
+    _newsLoadStateSubject.add(NewsLoadState.Loading);
+    notifyListeners();
+
     try {
+      http.Response apiResponse = await http.get(baseUrl + requestParams);
 
-      http.Response apiResponse = await http.get(baseUrl+requestParams);
-
-      if(apiResponse.statusCode != 200) {
-        // do something
+      if (apiResponse.statusCode != 200) {
+        updateNewsLoadState();
       } else {
-        
+        // Decode response body from json to map
+        var decodedJsonMap = json.decode(apiResponse.body);
+
+        // get the articles list
+        List decodedArticlesList = decodedJsonMap['articles'];
+
+        // if there is atleast one article, then add 
+        // the list to db
+        if (decodedArticlesList.length > 0)
+          await addArticlesToDb(decodedArticlesList);
+        else {
+          updateNewsLoadState();
+        }
       }
-
-    } catch(exception) {
+    } catch (exception) {
       print(exception);
-    } 
+    }
+  }
 
+  /// Add articles to database
+  Future addArticlesToDb(List articles) async {
 
+    // Iterate through the list of articles and 
+    // add each article to db.
+    articles.forEach((article) async {
+      // Check if the article already exists
+      bool isPresent = await _dbhelper.findMatchingArticle(article['url']);
+
+      if (!isPresent) {
+        // Add article to Database
+        await _dbhelper.saveArticle(article);
+      }
+    });
+  }
+
+  /// Fetch the cached articles from database
+  Future getArticlesFromDb({bool toRefresh = false}) async {
+
+    // Update NewsLoadState
+    _newsLoadStateSubject.add(NewsLoadState.Loading);
+    notifyListeners();
+
+    // Get articles from db
+    List<NewsArticle> cachedArticles = await _dbhelper.getArticles();
+
+    // If to refresh is selected then articles must be fetched again,
+    // after truncating the table.
+    if (toRefresh) {
+      bool internetPresent = await checkForInternet();
+
+      if (internetPresent) {
+
+        await _dbhelper.truncateTable();
+        await getArticlesFromApi();
+        await getArticlesFromDb(toRefresh: false);
+
+      } else {
+
+        // Store cached articles in master list if internet not present.
+        _articlesList = cachedArticles.reversed.toList();
+        updateNewsLoadState();
+      }
+    } else {
+      if (cachedArticles.length > 0) {
+        // Reversed to get the latest first
+        _articlesList = cachedArticles.reversed.toList();
+        updateNewsLoadState();
+      } else {
+        // Fetch articles if not present in database
+        await getArticlesFromApi();
+      }
+    }
+  }
+
+  /// Update NewsLoadState as per the availability of articles
+  void updateNewsLoadState() {
+    if (_articlesList.length > 0) {
+      _newsLoadStateSubject.add(NewsLoadState.Loaded);
+    } else {
+      _newsLoadStateSubject.add(NewsLoadState.Vacant);
+    }
+    notifyListeners();
   }
 }
